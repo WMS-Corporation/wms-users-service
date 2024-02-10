4c2eeba872f6c376eb70a974ac520117bedc7ef6
Martin0075 <martin.marcolini75@gmail.com>
2024-02-10 11:42:44 +0100

    chore(ci): setup github action with MongoDB

---
+++ run.bat
@@ -0,0 +1,4 @@
+set DB_NAME=WMS
+set DB_CONN_STRING=mongodb://127.0.0.1:27017
+set USER_COLLECTION=User
+npm start
--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,12 +1,15 @@
 const MongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv');
+
 
 const collections={};
 async function connectDB(){
+    dotenv.config();
     try{
-        const client = new MongoClient('mongodb://127.0.0.1:27017');
+        const client = new MongoClient(process.env.DB_CONN_STRING);
         await client.connect();
-        const db = client.db('WMS');
-        const usersCollection = db.collection('User');
+        const db = client.db(process.env.DB_NAME);
+        const usersCollection = db.collection(process.env.USER_COLLECTION);
         collections.users= usersCollection;
         console.log(`Successfully connected to database: ${db.databaseName} and collection: ${usersCollection.collectionName}`);
     }catch (error) {
