6791a4054c39a586a91421881cb7140b28c0ec43
Martin0075 <martin.marcolini75@gmail.com>
2024-02-12 17:08:50 +0100

    chore(index): Update CORS configuration

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors= require('cors');
-const {connectDB, collections} = require("./src/config/dbConnection");
+const {connectDB} = require("./src/config/dbConnection");
 const router= require('./src/routes/route');
 
+/*
+* Allow access from any subroute of http://localhost:3000
+* */
+let corsOptions = {
+    origin: /http:\/\/localhost:3000\/.*/
+};
 
 const app=express();
 app.disable("x-powered-by");
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(router);
 
 connectDB();
