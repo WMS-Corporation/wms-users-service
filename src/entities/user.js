f6bf239d8f97e93c09f13b221e90c8a90d6013e3
Martin0075 <martin.marcolini75@gmail.com>
2024-02-14 15:01:07 +0100

    chore(user): Add user entity

---
+++ src/entities/user.js
@@ -0,0 +1,60 @@
+class User{
+    constructor(codUser, username, password, name, surname, type) {
+        this._codUser = codUser;
+        this._username = username;
+        this._password = password;
+        this._name = name;
+        this._surname = surname;
+        this._type = type;
+    }
+
+
+    get codUser() {
+        return this._codUser;
+    }
+
+    set codUser(value) {
+        this._codUser = value;
+    }
+
+    get username() {
+        return this._username;
+    }
+
+    set username(value) {
+        this._username = value;
+    }
+
+    get password() {
+        return this._password;
+    }
+
+    set password(value) {
+        this._password = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get surname() {
+        return this._surname;
+    }
+
+    set surname(value) {
+        this._surname = value;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    set type(value) {
+        this._type = value;
+    }
+}
+module.exports = {User};
---
+++ test/user.test.js
@@ -0,0 +1,62 @@
+const { User } = require('../src/entities/user');
+describe('User testing', () => {
+    let user;
+
+    beforeEach(() => {
+        user = new User('000897', 'Martin0075', 'Bella', 'Martin', 'Marcolini', 'Admin');
+    });
+
+    it('should return the correct codUser', () => {
+        expect(user.codUser).toBe('000897');
+    });
+
+    it('should return the correct username', () => {
+        expect(user.username).toBe('Martin0075');
+    });
+
+    it('should return the correct password', () => {
+        expect(user.password).toBe('Bella');
+    });
+
+    it('should return the correct name', () => {
+        expect(user.name).toBe('Martin');
+    });
+
+    it('should return the correct surname', () => {
+        expect(user.surname).toBe('Marcolini');
+    });
+
+    it('should return the correct type', () => {
+        expect(user.type).toBe('Admin');
+    });
+
+    it('should set codUser correctly', () => {
+        user.codUser = '000678';
+        expect(user.codUser).toBe('000678');
+    });
+
+    it('should set username correctly', () => {
+        user.username = 'Jane0098';
+        expect(user.username).toBe('Jane0098');
+    });
+
+    it('should set password correctly', () => {
+        user.password = 'newpassword123';
+        expect(user.password).toBe('newpassword123');
+    });
+
+    it('should set name correctly', () => {
+        user.name = 'Jane';
+        expect(user.name).toBe('Jane');
+    });
+
+    it('should set surname correctly', () => {
+        user.surname = 'Smith';
+        expect(user.surname).toBe('Smith');
+    });
+
+    it('should set type correctly', () => {
+        user.type = 'operational';
+        expect(user.type).toBe('operational');
+    });
+});
