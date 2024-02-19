1885192128893655ded06a02d2c2d301a108a8e1
MichLadd <95909039+MichLadd@users.noreply.github.com>
2024-02-19 23:13:18 +0100

    chore(CI): check userRepository test on ubuntu 20

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -17,10 +17,12 @@ const loginUser= asyncHandler(async(req, res) =>{
                 user.token=generateToken(user.codUser);
                 return res.status(200).json({ message: 'Login successful', user });
             }
+            else{
+                return res.status(405).json({ message: 'DEBUG: Invalid password' });
+            }
         }
         else{
             return res.status(401).json({ message: 'Invalid email or password' });
-
         }
 
     }catch(error){
