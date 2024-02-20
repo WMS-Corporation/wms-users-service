7d1396502bd625e452fa5ed84c1b254a3ca1a563
Martin0075 <martin.marcolini75@gmail.com>
2024-02-20 18:25:16 +0100

    test(test): Updated userServices test

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const express = require('express');
 const cors= require('cors');
 const {connectDB} = require("./src/config/dbConnection");
 const router= require('./src/routes/route');
-
+const dotenv = require('dotenv');
 /*
 * Allow access from any subroute of http://localhost:3000
 * */
 let corsOptions = {
     origin: /http:\/\/localhost:3000\/.*/
 };
-
+dotenv.config();
 const app=express();
 app.disable("x-powered-by");
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(router);
+app.listen(process.env.PORT, () => console.info(`WMS-users-service is running`));
 
 connectDB();
 
