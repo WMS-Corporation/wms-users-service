9c40cb247704390c22d4f160f9ba81cb19681d9c
Martin0075 <martin.marcolini75@gmail.com>
2024-02-23 22:00:15 +0100

    test(test): Updated userRepository test

--- test/userRepository.test.js
+++ test/userRepository.test.js
@@ -5,21 +5,23 @@ const path = require("path");
 const fs = require("fs");
 const {User} = require("../src/entities/user");
 
-const username = 'Pietro0096'
-const expectedUser = {
-    "_codUser": "000867",
-    "_username": username,
-    "_password": "$2b$10$StPwi72JFnkcPLkgGdJYDOvA.M5Jrj7HTlyj8L6PQaetOyk87/6lW",
-    "_name": "Pietro",
-    "_surname": "Lelli",
-    "_type": "Operational"
-};
+
 
 describe('userRepository testing', () => {
     beforeAll(async () => {
         await connectDB()
     });
 
+    const username = 'Pietro0096'
+    const expectedUser = {
+        "_codUser": "000867",
+        "_username": username,
+        "_password": "$2b$10$StPwi72JFnkcPLkgGdJYDOvA.M5Jrj7HTlyj8L6PQaetOyk87/6lW",
+        "_name": "Pietro",
+        "_surname": "Lelli",
+        "_type": "Operational"
+    };
+    
     it("should create a new user",async () =>{
         const result=await createUser(new User("000867",username,"$2b$10$StPwi72JFnkcPLkgGdJYDOvA.M5Jrj7HTlyj8L6PQaetOyk87/6lW","Pietro","Lelli","Operational"))
         expect(result).toBeDefined()
